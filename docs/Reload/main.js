de340c23ccabe468ecab2f5741eb5dad2e5c124c
Yufeng Xie <yxie54@ucsc.edu>
2021-10-15 20:14:36 +0800

    Prototype Finished

--- docs/Reload/main.js
+++ docs/Reload/main.js
@@ -9,49 +9,46 @@ description = `
 // Sprites in the game
 characters = [
 	`
-llllll
-ll l l
-ll l l
-llllll
- l  l
- l  l
+ rrrr
+rr  rr
+rr  rr
+ rrrr
 `,
 	`
-llllll
-ll l l
-ll l l
-llllll
-ll  ll
-`,
-	`
-  lll
-ll l l
  llll
- l  l
-ll  ll
-`,
-	`
-  lll
 ll l l
- llll
-  ll
- l  l
- l  l
+llllll
 `,
 	`
-	 r
+ lll
+l l l
+lllll
+`,
+	`
+     r
 bbbbbr
 bbbbbr
 bbbbbr
-	 r
+     r
 `,
 ];
 
+// Game Constant
+const GAME = {
+	WIDTH: 100,
+	HEIGHT: 100,
+	AMMO_UI_POS_X: 50,
+	AMMO_UI_POS_Y: 90,
+	AMMO_CD: 40
+}
+
 // Different option in the game
 options = {
+	viewSize: { x: GAME.WIDTH, y: GAME.HEIGHT },
 	isPlayingBgm: true,
 	isReplayEnabled: true,
-	seed: 9,
+	seed: 11,
+	theme: "shapeDark"
 };
 
 // Create different Jsdoc for different elements in the game
@@ -67,6 +64,9 @@ let ammonRemain;
 let addAmmoCD;
 let reloadDuration;
 let shooting;
+let use;
+let load;
+
 
 // Where game update
 function update() {
@@ -75,40 +75,125 @@ function update() {
 		enemies = [];
 		ammo = [];
 		ammonRemain = 5;
-		addAmmoCD = 60;
+		addAmmoCD = GAME.AMMO_CD;
 		reloadDuration = 0;
 		shooting = new Boolean(true);
+		use = new Boolean(true);
+		load = new Boolean(false);
 		nextEnemyTicks = 0;
 		player = { pos: vec(50, 50), angle: -PI / 2 };
 	}
-	text(ammonRemain.toString(), 3, 10);
-	text(addAmmoCD.toString(), 3, 40);
 
+	// Add Ammo Remain UI and Particle effects
+	if (ammonRemain < 1) {
+		if (use && ammonRemain == 0) {
+			color("yellow");
+			particle(vec(GAME.AMMO_UI_POS_X - 20, GAME.AMMO_UI_POS_Y));
+			use = false;
+		}
+		color("transparent");
+	} else {
+		if (load && ammonRemain == 1) {
+			color("black");
+			particle(vec(GAME.AMMO_UI_POS_X - 20, GAME.AMMO_UI_POS_Y));
+			load = false;
+		}
+		color("black");
+	}
+	char("d", vec(GAME.AMMO_UI_POS_X - 20, GAME.AMMO_UI_POS_Y));
+	if (ammonRemain < 2) {
+		if (use && ammonRemain == 1) {
+			color("yellow");
+			particle(vec(GAME.AMMO_UI_POS_X - 10, GAME.AMMO_UI_POS_Y));
+			use = false;
+		}
+		color("transparent");
+	} else {
+		if (load && ammonRemain == 2) {
+			color("black");
+			particle(vec(GAME.AMMO_UI_POS_X - 10, GAME.AMMO_UI_POS_Y));
+			load = false;
+		}
+		color("black");
+	}
+	char("d", vec(GAME.AMMO_UI_POS_X - 10, GAME.AMMO_UI_POS_Y));
+	if (ammonRemain < 3) {
+		if (use && ammonRemain == 2) {
+			color("yellow");
+			particle(vec(GAME.AMMO_UI_POS_X, GAME.AMMO_UI_POS_Y));
+			use = false;
+		}
+		color("transparent");
+	} else {
+		if (load && ammonRemain == 3) {
+			color("black");
+			particle(vec(GAME.AMMO_UI_POS_X, GAME.AMMO_UI_POS_Y));
+			load = false;
+		}
+		color("black");
+	}
+	char("d", vec(GAME.AMMO_UI_POS_X, GAME.AMMO_UI_POS_Y));
+	if (ammonRemain < 4) {
+		if (use && ammonRemain == 3) {
+			color("yellow");
+			particle(vec(GAME.AMMO_UI_POS_X + 10, GAME.AMMO_UI_POS_Y));
+			use = false;
+		}
+		color("transparent");
+	} else {
+		if (load && ammonRemain == 4) {
+			color("black");
+			particle(vec(GAME.AMMO_UI_POS_X + 10, GAME.AMMO_UI_POS_Y));
+			load = false;
+		}
+		color("black");
+	}
+	char("d", vec(GAME.AMMO_UI_POS_X + 10, GAME.AMMO_UI_POS_Y));
+	if (ammonRemain < 5) {
+		if (use && ammonRemain == 4) {
+			color("yellow");
+			particle(vec(GAME.AMMO_UI_POS_X + 20, GAME.AMMO_UI_POS_Y));
+			use = false;
+		}
+		color("transparent");
+	} else {
+		if (load && ammonRemain == 5) {
+			color("black");
+			particle(vec(GAME.AMMO_UI_POS_X + 20, GAME.AMMO_UI_POS_Y));
+			load = false;
+		}
+		color("black");
+	}
+	char("d", vec(GAME.AMMO_UI_POS_X + 20, GAME.AMMO_UI_POS_Y));
+
+	// Reload Function
 	if (input.isPressed && ammonRemain < 5) {
 		shooting = false;
 		shotRange = 0;
 		addAmmoCD--
 		reloadDuration++;
-		if (addAmmoCD <= 0) {
+		if (addAmmoCD < 0) {
+			load = true;
 			play("coin");
 			ammonRemain++;
 			if (reloadDuration < 61) {
-				addAmmoCD = 60;
+				addAmmoCD = GAME.AMMO_CD - sqrt(difficulty);
 			} else if (reloadDuration > 61 && reloadDuration < 121) {
-				addAmmoCD = 40;
-			} else if (reloadDuration > 121 && reloadDuration < 181){
-				addAmmoCD = 20;
+				addAmmoCD = GAME.AMMO_CD - 20 - sqrt(difficulty);
+			} else if (reloadDuration > 121 && reloadDuration < 181) {
+				addAmmoCD = GAME.AMMO_CD - 40 - sqrt(difficulty);
 			} else {
-				addAmmoCD = 10;
+				addAmmoCD = GAME.AMMO_CD - 50 - sqrt(difficulty);
 			}
 		}
 	}
 	if (input.isJustReleased) {
 		shotRange = 30;
-		addAmmoCD = 60;
+		addAmmoCD = GAME.AMMO_CD - sqrt(difficulty);
 		reloadDuration = 0;
 		shooting = true;
 	}
+
 
 	// Enemy movespeed towards player
 	const scr = sqrt(difficulty);
@@ -142,7 +227,7 @@ function update() {
 		// @ts-ignore
 		const ta = player.pos.angleTo(te.pos);
 		const oa = wrap(ta - player.angle, -PI, PI);
-		const av = 0.035 * sqrt(difficulty);
+		const av = 0.1 * sqrt(difficulty);
 		if (abs(oa) < av) {
 			player.angle = ta;
 		} else {
@@ -151,13 +236,16 @@ function update() {
 	}
 
 	// Create Fire Line into the game
-	color("light_cyan");
+	color("white");
 	bar(player.pos, shotRange * 1.1, 2, player.angle, 0);
+
 	// Create player sprite into the game
 	color("blue");
-	char(addWithCharCode("a", floor(ticks / 20) % 2), player.pos, {
-		mirror: { x: abs(wrap(player.angle, -PI, PI)) < PI / 2 ? 1 : -1 },
-	});
+	char("a", player.pos);
+
+	// Create Player Gun into the game
+	color("cyan");
+	bar(player.pos, 5, 2, player.angle, 0);
 
 	// Conditions to remove the enemies from the world,
 	// and game over.
@@ -171,27 +259,30 @@ function update() {
 		}
 		e.pos.add(e.vel);
 
-		color("transparent");
-		const isCollidingWithPlayer = box(e.pos, 2).isColliding.char.a;
-		if (isCollidingWithPlayer) {
+		// Enemy Collider
+		color("black");
+		const c3 = char(addWithCharCode("b", floor(ticks / 30) % 2), e.pos, {
+			mirror: { x: abs(wrap(player.angle, -PI, PI)) < PI / 2 ? 1 : -1 },
+		}).isColliding;
+
+		// End Condition
+		if (c3.char.a || c3.char.a) {
 			play("lucky");
 			end();
 		}
 
-		color("black");
-		const c3 = char(addWithCharCode("c", floor(ticks / 30) % 2), e.pos, {
-			mirror: { x: abs(wrap(player.angle, -PI, PI)) < PI / 2 ? 1 : -1 },
-		}).isColliding.rect;
-		if (c3.light_cyan) {
+		// When enemy enter player fire range
+		if (c3.rect.white) {
 			if (ammonRemain > 0 && shooting) {
 				ammonRemain--;
+				use = true;
 				play("laser");
 				color("cyan");
 				bar(player.pos, shotRange, 4, player.angle, 0);
 				particle(player.pos, 20, 3, player.angle, 0);
 				color("red");
 				particle(e.pos);
-				addScore(1);
+				addScore(1, e.pos);
 				return true;
 			}
 		}
